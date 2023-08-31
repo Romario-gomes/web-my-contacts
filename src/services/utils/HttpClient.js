dc58ea0d0040d8fd998ebe95a4ad7250d61032fc
Romario Work <romario.g@estrela10.com.br>
2023-08-31 13:30:36 -0300

    Feat(throw error if api return failed): add

--- src/services/utils/HttpClient.js
+++ src/services/utils/HttpClient.js
@@ -5,11 +5,14 @@ class HttpClient {
     this.baseURL = baseURL;
   }
   async get(path) {
-    const response = await fetch(`${this.baseURL}${path}`);
-
     await delay(500);
 
-    return response.json();
+    const response = await fetch(`${this.baseURL}${path}`);
+
+    if(response.ok) {
+      return response.json();
+    }
+    throw new Error(`${response.status} - ${response.statusText}`);
   }
 }
 
